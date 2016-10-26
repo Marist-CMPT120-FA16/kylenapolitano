908660880825c58833b542f60147833aa23491ae
knapolitano <Kyle@148-100-167-251.FoxNet.marist.edu>
2016-10-26 16:45:05 -0400

    Started adding locations & help command. locations.js is not loading with the original file.

---
+++ locations.js
@@ -0,0 +1,51 @@
+
+
+function start(){
+	var description = "You are located in a vast desert alone and you need to find your way out. You should probably start looking around.";
+	return description;	
+}
+
+function cave(){
+	var description = "You have found a hidden cave!";
+	return description;
+}
+
+function cave1(){
+	var description = "While exploring this cave you think you hear something. Should you go further?";
+	return description;
+}
+
+function jeff(){
+	var description = "Upon further exploration you have found another human named Jeff!";
+	return description;	
+}
+
+function escape_cave(){
+	var description = "Jeff is very grateful for finding him. As your reward he proceeds to tell you how to escape the desert!";
+	return description;
+}
+
+function journey_middletown(){
+	var description = "Jeff wants to show you his town and his family so he can truly thank you. You two start your journey to Middletown(Jeff's home).";
+	return description;
+}
+
+function jeffs_warning(){
+	var description = "Jeff feels the need to warn you that the journey back to his hometown is very dangerous. The path is filled with robbers and wild creatures. Will you continue onward to Jeff's house?";
+	return description;
+}
+
+function journey_start(){
+	var description = "You have decided to continue onward to Jeff's house!";
+	return description;
+}
+
+function journey_woods(){
+	var description = "Your first challenge is to get through the woods. Jeff has already told you about the dangers that reside in there.";
+	return description;
+}
+
+function journey_woods_prefight(){
+	var description = "Upon entering the woods you get a bad feeling about this. Its too eerie for your liking.";
+	return description;
+}
